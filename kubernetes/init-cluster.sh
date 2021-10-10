7712ad7800d6448454c9d3b1d4b2fa0fbce5adca
Adam Johnston <adamjohnston@Adams-MacBook-Pro.local>
2021-10-09 23:39:08 -0600

    added deployment for mongo

--- kubernetes/init-cluster.sh
+++ kubernetes/init-cluster.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-helm upgrade --install -f ./helm/ingress/values.yaml ingress-thinksystemio ./helm/ingress --set name=thinksystemio
+# Databases
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm upgrade --install service-database-mongo bitnami/mongodb
+
+# Backends
 helm upgrade --install -f ./helm/deploy/values.yaml backend-proxy ./helm/deploy --set name=backend-proxy
 helm upgrade --install -f ./helm/deploy/values.yaml backend-orchestration ./helm/deploy --set name=backend-orchestration
+
+# Frontends
 helm upgrade --install -f ./helm/deploy/values.yaml frontend-loading ./helm/deploy --set name=frontend-loading
+
+# Ingress
+helm upgrade --install -f ./helm/ingress/values.yaml ingress-thinksystemio ./helm/ingress --set name=thinksystemio
